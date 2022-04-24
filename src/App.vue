7cbd35e38937d64497f355d7bf5a45ed10dd9e0b
KarapetyanNarek <nareklord@gmail.com>
2022-04-24 13:05:26 +0400

    Vue v1.0.0

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,48 @@
+
+<script>
+  import PostForm from "@/components/PostForm";
+  import PostList from "@/components/PostList";
+
+  export default {
+    components: {
+      PostList, PostForm
+    },
+    data() {
+      return {
+        posts: [
+          {id: 1, title: 'JavaScript', body: 'Description post'},
+          {id: 2, title: 'JavaScript 2', body: 'Description post 2'},
+          {id: 3, title: 'JavaScript 3', body: 'Description post 3'},
+        ],
+      }
+    },
+    methods: {
+      createPost(post) {
+        this.posts.push(post)
+      },
+    }
+  }
+</script>
+
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <div class="app">
+    <post-form
+        @create="createPost"
+    />
+    <post-list
+        :posts="posts"
+    />
+  </div>
 </template>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+  * {
+   margin: 0px;
+   padding: 0;
+   box-sizing: border-box;
+  }
 
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
-}
+  .app {
+    padding: 20px;
+  }
 </style>
